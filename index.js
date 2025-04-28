246150b4c286ec550219beb07b90a70df63308e7
Priya Kumari <122070124+priritgithub@users.noreply.github.com>
2025-04-28 17:49:34 +0000

    object creation

---
+++ index.js
@@ -0,0 +1,52 @@
+//object creation
+// let rectangle=
+// {
+//     length:1,
+//     breadt:2,
+//     draw: function()
+//     {
+//         console.log('drawing function');
+//     }
+// };
+
+
+
+
+//factory function
+// function createRectangle()
+// {
+//     let rectangle=
+//     {
+//         length:1,
+//         breadth:2,
+
+//         draw: function()
+//         {
+//             console.log("hello jee");
+//         }
+//     };
+//     return rectangle;
+// }
+// let rectangle1=createRectangle();
+// rectangle1.draw();
+// console.log(rectangle1.length);
+// console.log(rectangle1.breadth);
+
+
+
+
+//constructor function
+// function Rectangle(len,bre)
+// {
+//     this.length=len;
+//     this.breadth=bre,
+//     this.draw=function()
+//     {
+//         console.log('drawing');
+//     }
+// }
+// let rectangle1=new Rectangle(4,6);
+// rectangle1.draw();
+// console.log(rectangle1.length);
+// console.log(rectangle1.breadth);
+console.log(done);
